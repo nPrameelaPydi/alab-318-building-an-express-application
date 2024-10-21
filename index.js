9a354d4586cb5a23a2b894070473c0bd8f6b38ab
Prameela Pydi <nprameela.pydi@gmail.com>
2024-10-20 20:41:02 -0700

    rendered a view using custom template engine

--- index.js
+++ index.js
@@ -1,9 +1,31 @@
 const express = require("express");
 const app = express()
 const PORT = 3000;
-
-//const fs = require('fs');
-
 app.listen(PORT, () => {
     console.log(`appServer is listening on port: ${PORT}`);
 })
+
+const fs = require('fs');
+app.engine("ps", (filePath, options, callback) => {
+    fs.readFile(filePath, (err, content) => {
+        if (err) return callback(err);
+
+        const rendered = content
+            .toString()
+            .replaceAll("#title#", `${options.title}`)
+            .replace("#content#", `${options.content}`);
+
+        return callback(null, rendered);
+    });
+});
+
+app.set("views", "./views");
+app.set("view engine", "ps");
+
+app.get("/view-test", (req, res) => {
+    const options = {
+        title: "Alab-318-Express-Application",
+        content: "sometext dispaly",
+    };
+    res.render("index", options);
+});
